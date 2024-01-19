e0f5052ea59d2dc0c6ce930712bd4b7dadb34ba6
Harith Ramesh <harith.ramesh@sap.com>
2024-01-19 23:59:44 +0530

    Changing to custom db to default db

--- node/dbService.ts
+++ node/dbService.ts
@@ -1,29 +1,40 @@
 import { Client } from "pg";
-
+const DB_NAME = process.env.DB_NAME || "PlayersDB";
 export const dBConnection = new Client({
   host: "localhost",
   user: "postgres",
-  database: "myDB",
+  database: "postgres",
   password: "abcd1234",
   port: 5432,
 });
 
 dBConnection.connect((err) => {
   if (err) {
-    console.log(`Something went wrong while connecting database`);
+    console.log(`Something went wrong while connecting to database Server`);
   } else {
-    console.log("Database connection established successfully");
+    console.log("Database Server connection established successfully");
   }
 });
 export namespace dBService {
   export function createTable(tableName: string) {
-    const insertQuery = `CREATE TABLE IF NOT EXISTS ${tableName} (PlayerId uuid,  Name VARCHAR(100),PlayerType VARCHAR(100),Age int, Match int, HighestScore int,BattingAverage int,BowlingAverage int, Wickets int, BowlingEconomy int,PRIMARY KEY(PlayerId))`;
-
+    const insertQuery = `CREATE TABLE IF NOT EXISTS ${tableName} (
+      PlayerId uuid,
+      Name VARCHAR(100),
+      PlayerType VARCHAR(100),
+      Age int,
+      Match int,
+      HighestScore int,
+      BattingAverage int,
+      BowlingAverage int,
+      Wickets int,
+      BowlingEconomy int,
+      PRIMARY KEY(PlayerId)
+      )`;
     dBConnection.query(insertQuery, (err, res) => {
       if (err != null) {
         throw new Error(`Failed to create table reason ${err}`);
       }
-      console.log("Table created successfully");  
+      console.log("Table created successfully");
     });
   }
 }
