404b836493870ce2dab325f010530fedb85357d1
Harith <harith893@yahoo.com>
2025-07-11 14:50:41 +0530

    using dotenv to access env variables

--- node/index.ts
+++ node/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 import { dBConnection, dBService } from "./dbService";
 import { playerData } from "./interfaces";
 const app = express();
 const PORT = process.env.PORT || 5001;
 const TABLE = "Players";
 
+dotenv.config({path: "./.env"});
 app.use(cors());
 app.use(bodyParser.json());
 dBService.createTable(TABLE);
